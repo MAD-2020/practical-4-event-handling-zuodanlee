1f9862b29c9490daa122429b7b46d89201ceb83b
westwq <31364147+westwq@users.noreply.github.com>
2020-05-13 15:14:58 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/np/WhackAMole/Main2Activity.java
@@ -0,0 +1,96 @@
+package sg.edu.np.WhackAMole;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class Main2Activity extends AppCompatActivity {
+    /* Hint
+        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 8.
+        - The function doCheck() takes in button selected and computes a hit or miss and adjust the score accordingly.
+        - The functions readTimer() and placeMoleTimer() are to inform the user X seconds before starting and loading new mole.
+        - Feel free to modify the function to suit your program.
+    */
+
+
+
+    private void readyTimer(){
+        /*  HINT:
+            The "Get Ready" Timer.
+            Log.v(TAG, "Ready CountDown!" + millisUntilFinished/ 1000);
+            Toast message -"Get Ready In X seconds"
+            Log.v(TAG, "Ready CountDown Complete!");
+            Toast message - "GO!"
+            belongs here.
+            This timer countdown from 10 seconds to 0 seconds and stops after "GO!" is shown.
+         */
+    }
+    private void placeMoleTimer(){
+        /* HINT:
+           Creates new mole location each second.
+           Log.v(TAG, "New Mole Location!");
+           setNewMole();
+           belongs here.
+           This is an infinite countdown timer.
+         */
+    }
+    private static final int[] BUTTON_IDS = {
+        /* HINT:
+            Stores the 9 buttons IDs here for those who wishes to use array to create all 9 buttons.
+            You may use if you wish to change or remove to suit your codes.*/
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /*Hint:
+            This starts the countdown timers one at a time and prepares the user.
+            This also prepares the existing score brought over.
+            It also prepares the button listeners to each button.
+            You may wish to use the for loop to populate all 9 buttons with listeners.
+         */
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Log.v(TAG, "Current User Score: " + String.valueOf(advancedScore));
+
+
+        for(final int id : BUTTON_IDS){
+            /*  HINT:
+            This creates a for loop to populate all 9 buttons with listeners.
+            You may use if you wish to remove or change to suit your codes.
+            */
+        }
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+    private void doCheck(Button checkButton)
+    {
+        /* Hint:
+            Checks for hit or miss
+            Log.v(TAG, "Hit, score added!");
+            Log.v(TAG, "Missed, point deducted!");
+            belongs here.
+        */
+    }
+
+    public void setNewMole()
+    {
+        /* Hint:
+            Clears the previous mole location and gets a new random location of the next mole location.
+            Sets the new location of the mole.
+         */
+        Random ran = new Random();
+        int randomLocation = ran.nextInt(9);
+    }
+}
+
---
+++ app/src/main/java/sg/edu/np/WhackAMole/MainActivity.java
@@ -0,0 +1,77 @@
+package sg.edu.np.WhackAMole;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* Hint
+        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
+        - The function doCheck() takes in button selected and computes a hit or miss and adjust the score accordingly.
+        - The function doCheck() also decides if the user qualifies for the advance level and triggers for a dialog box to ask for user to decide.
+        - The function nextLevelQuery() builds the dialog box and shows. It also triggers the nextLevel() if user selects Yes or return to normal state if user select No.
+        - The function nextLevel() launches the new advanced page.
+        - Feel free to modify the function to suit your program.
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "Finished Pre-Initialisation!");
+
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        setNewMole();
+        Log.v(TAG, "Starting GUI!");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.v(TAG, "Paused Whack-A-Mole!");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.v(TAG, "Stopped Whack-A-Mole!");
+        finish();
+    }
+
+    private void doCheck(Button checkButton) {
+        /* Checks for hit or miss and if user qualify for advanced page.
+            Triggers nextLevelQuery().
+         */
+    }
+
+    private void nextLevelQuery(){
+        /*
+        Builds dialog box here.
+        Log.v(TAG, "User accepts!");
+        Log.v(TAG, "User decline!");
+        Log.v(TAG, "Advance option given to user!");
+        belongs here*/
+    }
+
+    private void nextLevel(){
+        /* Launch advanced page */
+    }
+
+    private void setNewMole() {
+        Random ran = new Random();
+        int randomLocation = ran.nextInt(3);
+    }
+}
